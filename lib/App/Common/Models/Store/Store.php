1f292b5b94a0c1cda456e737e34b2f547754759e
handsomegyr <handsomegyr@126.com>
2019-11-26 11:11:39 +0800

    门店增加额外的字段

--- lib/App/Common/Models/Store/Store.php
+++ lib/App/Common/Models/Store/Store.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Common\Models\Store;
 
 use App\Common\Models\Base\Base;
@@ -9,5 +10,23 @@ class Store extends Base
     function __construct()
     {
         $this->setModel(new \App\Common\Models\Store\Mysql\Store());
+    }
+
+    public function getImagePath($baseUrl, $image, $x = 0, $y = 0)
+    {
+        $uploadPath = $this->getUploadPath();
+        $xyStr = "";
+        if (!empty($x)) {
+            $xyStr .= "&w={$x}";
+        }
+        if (!empty($y)) {
+            $xyStr .= "&h={$y}";
+        }
+        return "{$baseUrl}service/file/index?id={$image}&upload_path={$uploadPath}{$xyStr}";
+    }
+
+    public function getUploadPath()
+    {
+        return trim("store", '/');
     }
 }
