647b02c73c3981912dd7c15fe0f1b4c79d245837
handsomegyr <handsomegyr@126.com>
2020-02-25 18:06:14 +0800

    积分组件升级

--- lib/App/Backend/Submodules/Points/Models/Rule.php
+++ lib/App/Backend/Submodules/Points/Models/Rule.php
@@ -5,4 +5,22 @@ namespace App\Backend\Submodules\Points\Models;
 class Rule extends \App\Common\Models\Points\Rule
 {
     use \App\Backend\Models\Base;
+
+    /**
+     * 获取所有列表
+     *
+     * @return array
+     */
+    public function getAll($field = '_id')
+    {
+        $query = $this->getQuery();
+        $sort = $this->getDefaultSort();
+        $ret = $this->findAll($query, $sort);
+
+        $list = array();
+        foreach ($ret as $item) {
+            $list[$item[$field]] = $item['item'];
+        }
+        return $list;
+    }
 }
