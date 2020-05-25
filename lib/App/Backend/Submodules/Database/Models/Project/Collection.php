cdb31125b7a6651b5b4eeb3ef0560e845cd1a29c
handsomegyr <handsomegyr@126.com>
2020-05-25 18:22:43 +0800

    idb管理

--- lib/App/Backend/Submodules/Database/Models/Project/Collection.php
+++ lib/App/Backend/Submodules/Database/Models/Project/Collection.php
@@ -5,4 +5,21 @@ namespace App\Backend\Submodules\Database\Models\Project;
 class Collection extends \App\Common\Models\Database\Project\Collection
 {
     use \App\Backend\Models\Base;
+
+    /**
+     * 获取所有数据库表列表
+     *
+     * @return array
+     */
+    public function getAll()
+    {
+        $query = $this->getQuery();
+        $sort = $this->getDefaultSort();
+        $ret = $this->findAll($query, $sort);
+        $list = array();
+        foreach ($ret as $item) {
+            $list[$item['_id']] = $item['name'];
+        }
+        return $list;
+    }
 }
