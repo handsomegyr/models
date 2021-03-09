6089eb51f803c971170d9aad27de2a5b375cb3b0
handsomegyr <handsomegyr@126.com>
2021-03-09 14:54:41 +0800

    修改

--- lib/App/Common/Models/Qyweixin/Notification/Task.php
+++ lib/App/Common/Models/Qyweixin/Notification/Task.php
@@ -26,4 +26,29 @@ class Task extends Base
         }
         return $options;
     }
+
+    public function checkChangemsginfoCallbackIsValid($changemsginfo_callback)
+    {
+        // 如果没有设置回调函数的话 那么就直接返回
+        if (empty($changemsginfo_callback)) {
+            return true;
+        } else {
+            $changemsginfo_callback_info = \json_decode($changemsginfo_callback, true);
+            // 如果不是有效合法的json格式的话就直接返回
+            if (empty($changemsginfo_callback_info)) {
+                return false;
+            } else {
+                $className = empty($changemsginfo_callback_info['class']) ? "" : trim($changemsginfo_callback_info['class']);
+                $methodName = empty($changemsginfo_callback_info['method']) ? "" : trim($changemsginfo_callback_info['method']);
+
+                if (empty($className)) {
+                    return is_callable($methodName);
+                } else {
+                    $anObject  = new $className();
+                    $methodVariable  = array($anObject,  $methodName);
+                    return is_callable($methodVariable);
+                }
+            }
+        }
+    }
 }
--- lib/App/Common/Models/Qyweixin/Notification/TaskLog.php
+++ lib/App/Common/Models/Qyweixin/Notification/TaskLog.php
@@ -11,4 +11,27 @@ class TaskLog extends Base
     {
         $this->setModel(new \App\Common\Models\Qyweixin\Mysql\Notification\TaskLog());
     }
+
+    public function changeMsgInfo($taskLog, $msgInfo)
+    {
+        // 如果没有设置回调函数的话 那么就直接返回
+        if (empty($taskLog['changemsginfo_callback'])) {
+            return $msgInfo;
+        } else {
+            $changemsginfo_callback_info = \json_decode($taskLog['changemsginfo_callback'], true);
+            // 如果不是有效合法的json格式的话就直接返回
+            if (empty($changemsginfo_callback_info)) {
+                return $msgInfo;
+            } else {
+                $className = empty($changemsginfo_callback_info['class']) ? "" : trim($changemsginfo_callback_info['class']);
+                $methodName = empty($changemsginfo_callback_info['method']) ? "" : trim($changemsginfo_callback_info['method']);
+                $userid = $taskLog['userid'];
+                if (empty($className)) {
+                    return call_user_func($methodName, $userid, $msgInfo);
+                } else {
+                    return call_user_func(array($className, $methodName), $userid, $msgInfo);
+                }
+            }
+        }
+    }
 }
