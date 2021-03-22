0ba879d23adcce115bc39b09a5737f4bc6c8942b
handsomegyr <handsomegyr@126.com>
2021-03-22 15:50:50 +0800

    修改

--- lib/App/Qyweixin/Services/QyService.php
+++ lib/App/Qyweixin/Services/QyService.php
@@ -1118,28 +1118,28 @@ class QyService
         try {
             switch ($match['linkedcorp_msg_type']) {
                 case 'text':
-                    $objMsg = new \Qyweixin\Model\LinkedcorpMsg\Text($agentid, $linkedcorpMsgInfo['description']);
+                    $objMsg = new \Qyweixin\Model\Linkedcorp\Message\Text($agentid, $linkedcorpMsgInfo['description']);
                     break;
                 case 'voice':
                     $media_id = $this->getMediaId4LinkedcorpMsg('voice', $linkedcorpMsgInfo);
-                    $objMsg = new \Qyweixin\Model\LinkedcorpMsg\Voice($agentid, $media_id);
+                    $objMsg = new \Qyweixin\Model\Linkedcorp\Message\Voice($agentid, $media_id);
                     break;
                 case 'video':
                     $media_id = $this->getMediaId4LinkedcorpMsg('video', $linkedcorpMsgInfo);
-                    $objMsg = new \Qyweixin\Model\LinkedcorpMsg\Video($agentid, $media_id);
+                    $objMsg = new \Qyweixin\Model\Linkedcorp\Message\Video($agentid, $media_id);
                     $objMsg->title = $linkedcorpMsgInfo['title'];
                     $objMsg->description = $linkedcorpMsgInfo['description'];
                     break;
                 case 'file':
                     $media_id = $this->getMediaId4LinkedcorpMsg('file', $linkedcorpMsgInfo);
-                    $objMsg = new \Qyweixin\Model\LinkedcorpMsg\File($agentid, $media_id);
+                    $objMsg = new \Qyweixin\Model\Linkedcorp\Message\File($agentid, $media_id);
                     break;
                 case 'image':
                     $media_id = $this->getMediaId4LinkedcorpMsg('image', $linkedcorpMsgInfo);
-                    $objMsg = new \Qyweixin\Model\LinkedcorpMsg\Image($agentid, $media_id);
+                    $objMsg = new \Qyweixin\Model\Linkedcorp\Message\Image($agentid, $media_id);
                     break;
                 case 'textcard':
-                    $objMsg = new \Qyweixin\Model\LinkedcorpMsg\TextCard($agentid, $linkedcorpMsgInfo['title'], $linkedcorpMsgInfo['description'], $linkedcorpMsgInfo['url']);
+                    $objMsg = new \Qyweixin\Model\Linkedcorp\Message\TextCard($agentid, $linkedcorpMsgInfo['title'], $linkedcorpMsgInfo['description'], $linkedcorpMsgInfo['url']);
                     $objMsg->btntxt = $linkedcorpMsgInfo['btntxt'];
                     break;
                 case 'news':
@@ -1149,7 +1149,7 @@ class QyService
                     $modelLinkedcorpMsgNews = new \App\Qyweixin\Models\LinkedcorpMsg\News();
                     $articles1 = $modelLinkedcorpMsgNews->getArticlesByLinkedcorpMsgId($linkedcorpMsgInfo['_id'], 'news', $isFirst);
                     $articles = array_merge($articles, $articles1);
-                    $objMsg = new \Qyweixin\Model\LinkedcorpMsg\News($agentid, $articles);
+                    $objMsg = new \Qyweixin\Model\Linkedcorp\Message\News($agentid, $articles);
                     break;
                 case 'mpnews':
                     // 获取图文列表
@@ -1164,13 +1164,13 @@ class QyService
                         $article['thumb_media_id'] = $res4ThumbMedia['media_id'];
                         unset($article['thumb_media']);
                     }
-                    $objMsg = new \Qyweixin\Model\LinkedcorpMsg\Mpnews($agentid, $articles);
+                    $objMsg = new \Qyweixin\Model\Linkedcorp\Message\Mpnews($agentid, $articles);
                     break;
                 case 'markdown':
-                    $objMsg = new \Qyweixin\Model\LinkedcorpMsg\Markdown($agentid, $linkedcorpMsgInfo['description']);
+                    $objMsg = new \Qyweixin\Model\Linkedcorp\Message\Markdown($agentid, $linkedcorpMsgInfo['description']);
                     break;
                 case 'miniprogram_notice':
-                    $objMsg = new \Qyweixin\Model\LinkedcorpMsg\MiniprogramNotice($linkedcorpMsgInfo['appid'], $linkedcorpMsgInfo['title']);
+                    $objMsg = new \Qyweixin\Model\Linkedcorp\Message\MiniprogramNotice($linkedcorpMsgInfo['appid'], $linkedcorpMsgInfo['title']);
                     $objMsg->page = $linkedcorpMsgInfo['pagepath'];
                     $objMsg->description = $linkedcorpMsgInfo['description'];
                     $objMsg->emphasis_first_item = $linkedcorpMsgInfo['emphasis_first_item'];
@@ -1184,7 +1184,7 @@ class QyService
             $objMsg->touser = array($ToUserName);
             $objMsg->toall = intval($linkedcorpMsgInfo['toall']);
             $objMsg->safe = intval($linkedcorpMsgInfo['safe']);
-            $linkedcorpmsg = $objQyWeixin->getLinkedcorpMessageManager()->send($objMsg);
+            $linkedcorpmsg = $objQyWeixin->getLinkedcorpManager()->getMessageManager()->send($objMsg);
             if (!empty($linkedcorpmsg['errcode'])) {
                 throw new \Exception($linkedcorpmsg['errmsg'], $linkedcorpmsg['errcode']);
             }
