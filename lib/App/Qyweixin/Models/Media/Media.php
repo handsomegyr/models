e7ab2c69e17463bac5b241f8ce973ec680f3effd
handson@intonead.com <gyr@3332502!>
2022-03-31 13:15:41 +0800

    增加媒体的方法

--- lib/App/Qyweixin/Models/Media/Media.php
+++ lib/App/Qyweixin/Models/Media/Media.php
@@ -5,6 +5,35 @@ namespace App\Qyweixin\Models\Media;
 class Media extends \App\Common\Models\Qyweixin\Media\Media
 {
 
+    /**
+     * 获取信息
+     *
+     * @return array
+     */
+    public function getInfoByMedia($provider_appid, $authorizer_appid, $agentid, $media, $type)
+    {
+        $info = $this->findOne(array(
+            'media' => $media,
+            'type' => $type,
+            'agentid' => $agentid,
+            'authorizer_appid' => $authorizer_appid,
+            'provider_appid' => $provider_appid
+        ));
+        return $info;
+    }
+
+    public function createMedia($provider_appid, $authorizer_appid, $agentid, $name, $type, $media)
+    {
+        $data = array();
+        $data['provider_appid'] = $provider_appid;
+        $data['authorizer_appid'] = $authorizer_appid;
+        $data['agentid'] = $agentid;
+        $data['name'] = $name;
+        $data['type'] = $type;
+        $data['media'] = $media;
+        return $this->insert($data);
+    }
+
     public function recordMediaId($id, $res, $now)
     {
         $updateData = array();
